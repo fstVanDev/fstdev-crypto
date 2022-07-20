4b3f4bd98d7d0d8faf3887a01b8232f2792ff16a
Khandoga Kirill <fstDev.git@gmail.com>
2022-07-20 14:34:55 +0600

    Revert "start merge"
    This reverts commit 131e057024b6235b6dee38e32edae2db9d5a1751.

--- client/src/AdminPanel/Admin.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Admin = () => {
-  
-    return <h1 style={{color: 'white'}}>Admin Panel</h1>;
-  
-}
-
-export default Admin;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,13 @@
-import { Navbar, Welcome } from "./components";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Admin from "./AdminPanel/Admin";
-
-const Home = () => {
-  return (
-    <div className="gradient-bg-welcome">
-      <Navbar />
-      <Welcome />
-    </div>
-  );
-};
-
+import { Navbar, Welcome } from './components'
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
 
-export default App;
+   return (
+      <div className='gradient-bg-welcome'>
+            <Navbar />
+            <Welcome />
+      </div>
+   )
+}
+
+export default App
